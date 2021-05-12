b95c415ed234df9183bf6e0552a35a78d7c119a5
Ivy Stephenson <istephensondev@gmail.com>
2021-05-12 17:34:07 -0400

    commit

--- server.js
+++ server.js
@@ -24,6 +24,18 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const Sequelize = require('sequelize');
+require('dotenv').config();
+// create connection to our db
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     });
+// module.exports = sequelize;
+
 //middlewear
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
